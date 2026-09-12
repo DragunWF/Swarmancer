a06a21369e1b1bfdc227482ab4a34ed4dc88f962
DragunWF <plarisanmarc2@gmail.com>
2026-09-12 12:05:49 +0800

    feat: add grunt sprite reference to asset loader and enemy entity

--- entities/enemies.py
+++ entities/enemies.py
@@ -10,7 +10,7 @@ class Grunt:
         # Slower max_speed than Boid (350.0) so they can be kited.
         self.physics = Physics(max_speed=150.0, mass=1.5)
         # Red-ish color, slightly larger scale than boid
-        self.graphics = Graphics(color=(220, 50, 50), scale=4.0)
+        self.graphics = Graphics(sprite_ref="grunt", color=(220, 50, 50), scale=4.0)
         self.collider = Collider(radius=4.0)
         
         self.is_enemy = True
